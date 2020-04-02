98f97d5750d56542caa4790a2e1ccc238426693c
Piotro <piotro888@wp.pl>
2020-04-02 12:41:50 +0200

    Added BuildingGeneratorObject and BuildingRenderrer script template

---
+++ Assets/Scripts/Building/BuildingRenderrer.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildingRenderrer : MonoBehaviour
+{
+    public GameObject[] gameObjects;
+    public int[] building;
+
+    public void render(){
+
+    }
+}
