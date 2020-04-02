3816a391c5e9228b9f1c7f3a4d400b677c92bb8a
Piotro <piotro888@wp.pl>
2020-04-02 13:05:53 +0200

    Building render function

--- Assets/Scripts/Building/BuildingRenderrer.cs
+++ Assets/Scripts/Building/BuildingRenderrer.cs
@@ -1,13 +1,31 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class BuildingRenderrer : MonoBehaviour
 {
     public GameObject[] gameObjects;
-    public int[] building;
+    public int[,] building = {
+        {0, 3, 1, 1, 1, 0},
+        {0, 0, 0, 3, 0, 0}
+    };
+    
+    void Start(){
+        render(); //only for testing
+    }
 
+    ///Render building based on public building array.
     public void render(){
+        for(int i=building.GetLength(0)-1; i>=0; i--){
+            for(int j=0; j<building.GetLength(1); j++){
+                createBlock(building[i,j], i, j);    
+            }
+        }
+    }
 
+    void createBlock(int id, int x, int y){
+        Vector3 blockPosition = transform.position + new Vector3(y, x, 0);
+        Instantiate(
+            gameObjects[id],
+            blockPosition,
+            Quaternion.identity);
     }
 }
