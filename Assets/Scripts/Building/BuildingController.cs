02de3254575741ddbf2cdc90fc5de3392d6ac78e
Piotro <piotro888@wp.pl>
2020-04-02 16:34:00 +0200

    Check if line valid function

--- Assets/Scripts/Building/BuildingController.cs
+++ Assets/Scripts/Building/BuildingController.cs
@@ -3,8 +3,18 @@
 public class BuildingController : MonoBehaviour
 {
     public BuildingRenderrer buildingRenderrer;
+    public int minInLine;
 
     void Start(){
         buildingRenderrer.render(); //render building on start
+    }
+
+    public bool checkIfLineValid(int line){
+        int count = 0;
+        for(int i=0; i<buildingRenderrer.building.GetLength(1); i++){
+            if(buildingRenderrer.building[line, i] != 0)
+                count++;
+        }
+        return (count >= minInLine ? true : false);
     }
 }
