01b75be707ae19461512cafb5fdc094078119b3e
Piotro <piotro888@wp.pl>
2020-04-03 17:49:56 +0200

    Added bomb prefab

---
+++ Assets/Scripts/BombController.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Diagnostics;
+
+public class BombController : MonoBehaviour
+{
+    Stopwatch stopwatch;
+
+    void Start(){
+        stopwatch.Start();
+    }
+
+    
+    void FixedUpdate(){
+        if(stopwatch.ElapsedMilliseconds > 1000){
+
+        }
+    }
+}
