46857fdf348fda06d0f29dfce7d1bf51e52401f9
Piotro <piotro888@wp.pl>
2020-04-04 19:00:42 +0200

    Created template raycast script

---
+++ Assets/Scripts/MouseController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseController : MonoBehaviour
+{
+    void Update(){
+        RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+ 
+        if(hit.collider != null) {
+            Debug.Log ("Target Position: " + hit.collider.gameObject.transform.position);
+        } else {
+            Debug.Log("NULL");
+        }
+ 
+    }
+}
