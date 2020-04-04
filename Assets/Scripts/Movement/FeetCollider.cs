799ea0b5b729d5b8683153d3de6aeb6f4682f2ca
BananekProgramista <michal.banaszkiewicz@onet.eu>
2020-04-04 16:39:01 +0200

    Movement

---
+++ Assets/Scripts/Movement/FeetCollider.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeetCollider : MonoBehaviour
+{
+    public bool isLadder=false;
+    void OnTriggerStay2D(Collider2D obj){
+        if(obj.gameObject.tag=="Ground") isLadder=false;
+        else isLadder=true;
+    }
+}
