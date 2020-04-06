c561df0acf506638b5b69d68cf768e42b58e6626
Piotro <piotro888@wp.pl>
2020-04-06 22:33:23 +0200

    Added splash animation

---
+++ Assets/Scripts/SplashScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SplashScript : MonoBehaviour
+{
+    Stopwatch stopwatch = new Stopwatch();
+    void Start(){
+        stopwatch.Start();
+    }
+
+    void Update(){
+        if(stopwatch.ElapsedMilliseconds > 4000){
+            stopwatch.Stop();
+            Application.LoadLevelAsync("StartMenu");
+        }
+    }
+}
