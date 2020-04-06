4ea158c035c1fcf32492d61c8b2f46298122d55c
Piotro <piotro888@wp.pl>
2020-04-06 12:41:36 +0200

    Added timer and score

--- Assets/Scripts/TimerControl.cs
+++ Assets/Scripts/TimerControl.cs
@@ -1,18 +1,33 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System;
+using TMPro;
+using System.Diagnostics;
 using UnityEngine;
 
 public class TimerControl : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    public TMPro.TextMeshProUGUI timerText;
+    public TMPro.TextMeshProUGUI scoreText;
+    public TMPro.TextMeshProUGUI fpsText;
+    public GameObject buildingGen;
+    public int endTime = 180;
+    Stopwatch stopwatch = new Stopwatch();
+
+    void Start() {
+        stopwatch.Start();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        int elapsedSeconds = (int) Mathf.Floor(stopwatch.ElapsedMilliseconds / 1000f);
+        fpsText.text = Mathf.Ceil(1.0f / Time.smoothDeltaTime) + " FPS";
+        string secstring = String.Format("{0:00}", (endTime - elapsedSeconds)%60);
+        timerText.text = (endTime - elapsedSeconds)/60 + ":" + secstring;
+        scoreText.text = ""+calcScore();
+    }
+
+    int calcScore(){
+        int elapsedSeconds = (int) Mathf.Floor(stopwatch.ElapsedMilliseconds / 1000f);
+        int timepart = elapsedSeconds*10;
+        int floorpart = buildingGen.GetComponent<BuildingRenderrer>().buildingHeight * 100;
+        return timepart + floorpart;
     }
 }
