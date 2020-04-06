3bf8eb9dfa86d2fe9ef9ae2aa80b3c14d5e6198c
Piotro <piotro888@wp.pl>
2020-04-06 18:30:29 +0200

    Random sprite selection

---
+++ Assets/Scripts/Building/RandomSprite.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomSprite : MonoBehaviour
+{
+    public Sprite[] sprites;
+    System.Random random = new System.Random();
+
+    void Start(){
+        int rand = random.Next(0, sprites.Length);
+        GetComponent<SpriteRenderer>().sprite = sprites[rand];
+    }
+}
