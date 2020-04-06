92d527127e0f55ab6c6fbbb2df67b49b78a73ca8
Piotro <piotro888@wp.pl>
2020-04-06 08:09:11 +0200

    Added inventory

---
+++ Assets/Scripts/InvDisp.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class InvDisp : MonoBehaviour
+{   
+    public TMPro.TextMeshProUGUI text1;
+    public TMPro.TextMeshProUGUI text2;
+    public GameObject playerObj;
+    
+    Movement player;
+
+    void Start(){
+        player = playerObj.GetComponent<Movement>();
+    }
+
+    void Update() {
+        text1.text = ""+player.getConstructionElements();
+        text2.text = ""+player.getLadders();
+    }
+}
